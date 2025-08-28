de47013ee57d0e50ad13fc1033f54923a55e99f6
Aman-jha12 <ajha82703@gmail.com>
2025-08-28 21:12:30 +0530

    Updated app.ts

--- apps/api/src/core/app.ts
+++ apps/api/src/core/app.ts
@@ -4,6 +4,8 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import { router as authRoutes } from "../api/auth/auth.routes";
+import { router as staffRoutes } from "../api/staff/staff.routes"; // 
+
 
 export const createApp = () => {
   const app = express();
@@ -16,7 +18,9 @@ export const createApp = () => {
 
   app.get("/health", (_req, res) => res.json({ ok: true }));
 
+  // API Routes
   app.use("/api/v1/auth", authRoutes);
+  app.use("/api/v1/staff", staffRoutes); // 2. Use staff routes
 
   // 404
   app.use((_req, res) => res.status(404).json({ error: "Not found" }));
